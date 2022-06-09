74549ac2cc31d39f0caf9887b7f570b2f3536ad8
Nicklas <nicklas_tp@hotmail.com>
2022-06-09 13:18:34 +0200

    commit

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -41,6 +41,36 @@ const Product = () => {
           </div>
 
         </div>  
+
+        <h3 className={styles.choose}>Choose additional ingredients</h3>
+        <div className={styles.ingredients}>
+          <div className={styles.option}>
+            <input type="checkbox" id="double" name="double" className={styles.checkbox} />
+            <label htmlFor="double">Double ingredients</label>
+          </div>
+          <div className={styles.option}>
+            <input type="checkbox" id="extraCheese" name="extraCheese" className={styles.checkbox} />
+            <label htmlFor="extraCheese">Extra cheese</label>
+          </div>
+          <div className={styles.option}>
+            <input type="checkbox" id="chili" name="chili" className={styles.checkbox} />
+            <label htmlFor="chili">Chili sauce</label>
+          </div>
+          <div className={styles.option}>
+            <input type="checkbox" id="garlic" name="garlic" className={styles.checkbox} />
+            <label htmlFor="garlic">Garlic sauce</label>
+          </div>
+          <div className={styles.option}>
+            <input type="checkbox" id="wholeGrain" name="wholeGrain" className={styles.checkbox} />
+            <label htmlFor="wholeGrain">Whole grain bread</label>
+          </div>
+        </div>
+
+        <div className={styles.add}>
+          <input type="number" defaultValue={1} className={styles.quantity} />
+          <button className={styles.button}>Add to cart</button> 
+        </div>
+
       </div>
     </div>
   )
